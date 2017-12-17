f8cbe36174f071bb98d0626fc915ad8d5b4e2159
Ryan <ryan.manzer@gmail.com>
2017-12-17 09:51:54 -0800

    Modifications to improve Docker container usage

---
+++ web/gunicorn.conf.py
@@ -0,0 +1,5 @@
+"""
+Gunicorn Configuration File:
+---------------------------------
+ This file describes the parameters used to configure the gunicorn web server which will handle passing requests between the NGINX proxy server and the Django application.
+"""
