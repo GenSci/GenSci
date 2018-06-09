342159beb403d7f85579b1ffbf06fdef77687359
Ryan <ryan.manzer@gmail.com>
2018-06-08 18:49:50 -0700

    Further Pi related tweaks to Ngxinx container

--- django/run_web.sh
+++ django/run_web.sh
@@ -6,7 +6,7 @@
 ###################################################################
 
 # We wait 30 seconds to allow the database server to file up
-sleep 30
+sleep 10
 # Creating migrations to catch any database changes we've made in our Django
 #  applciation.
 eval 'python3 manage.py makemigrations'
